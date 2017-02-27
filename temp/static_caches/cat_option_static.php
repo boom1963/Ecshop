f31ec484cd6478c05e6f829ca80bf42b823f7185
JimmyChoi <jimmychoi1982@gmail.com>
2017-02-27 14:55:20 +0900

    Temp文件上传

---
+++ temp/static_caches/cat_option_static.php
@@ -0,0 +1,36 @@
+<?php
+$data = array (
+  1 => 
+  array (
+    'cat_id' => '1',
+    'cat_name' => '电子产品',
+    'measure_unit' => '',
+    'parent_id' => '0',
+    'is_show' => '1',
+    'show_in_nav' => '0',
+    'grade' => '0',
+    'sort_order' => '50',
+    'has_children' => '0',
+    'goods_num' => '2',
+    'level' => 0,
+    'id' => '1',
+    'name' => '电子产品',
+  ),
+  2 => 
+  array (
+    'cat_id' => '2',
+    'cat_name' => '测试分类',
+    'measure_unit' => '12',
+    'parent_id' => '0',
+    'is_show' => '1',
+    'show_in_nav' => '0',
+    'grade' => '0',
+    'sort_order' => '50',
+    'has_children' => '0',
+    'goods_num' => 0,
+    'level' => 0,
+    'id' => '2',
+    'name' => '测试分类',
+  ),
+);
+?>
---
+++ temp/static_caches/cat_pid_releate.php
@@ -0,0 +1,30 @@
+<?php
+$data = array (
+  0 => 
+  array (
+    'cat_id' => '1',
+    'cat_name' => '电子产品',
+    'measure_unit' => '',
+    'parent_id' => '0',
+    'is_show' => '1',
+    'show_in_nav' => '0',
+    'grade' => '0',
+    'sort_order' => '50',
+    'has_children' => '0',
+    'goods_num' => '2',
+  ),
+  1 => 
+  array (
+    'cat_id' => '2',
+    'cat_name' => '测试分类',
+    'measure_unit' => '12',
+    'parent_id' => '0',
+    'is_show' => '1',
+    'show_in_nav' => '0',
+    'grade' => '0',
+    'sort_order' => '50',
+    'has_children' => '0',
+    'goods_num' => 0,
+  ),
+);
+?>
